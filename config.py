0fe706d4f47760078c070dab66d6d8e916639de3
Gustavo de Almeida Pacheco <101198664+Gusta9s@users.noreply.github.com>
2026-09-24 17:48:02 -0300

    Remove hardcoded secret from config.py
    Removed hardcoded secret key from configuration.

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ import os
 # Instanciamos nossas variáveis de ambiente essenciais para bom funcionamento no ambiente local, quanto de produção.
 class Config():
     CSRF_ENABLE = True
-    SECRET = '9T3P7X5K6H2J'
     TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     APP = None
